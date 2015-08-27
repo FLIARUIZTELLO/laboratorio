db4cf56f30f7bb3fd84e1769494d1e867fc98192
Christian <Christian@MININT-PITSIEV>
2015-08-26 22:51:05 -0500

    DESDE LA DELL aumente crrb en la dell

--- POM_04_WEB/src/main/java/ec/infocenter/pom_04_web/jsfclases/ImalabCabeceraController.java
+++ POM_04_WEB/src/main/java/ec/infocenter/pom_04_web/jsfclases/ImalabCabeceraController.java
@@ -18,7 +18,7 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
-//crrb
+//crrb2
 @Named("imalabCabeceraController")
 @SessionScoped
 public class ImalabCabeceraController implements Serializable {
